27800e6a8a6e027e69eb21c546d64e46ce103249
Ranjith Kumar K.N <70890713+Rangow4562@users.noreply.github.com>
2022-01-06 16:45:29 +0530

    Update app.py

--- flask_registration/flaskapp/app.py
+++ flask_registration/flaskapp/app.py
@@ -119,4 +119,4 @@ def delete_registration(id):
  
 # starting the app
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
